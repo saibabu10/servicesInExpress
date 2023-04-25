9f5a46db57071bb73da16f570117f65bc1b2792c
sai babu <saibabu@sais-MacBook-Air-2.local>
2023-04-25 11:28:17 +0530

    updated code in servicePages

--- servicePages.js
+++ servicePages.js
@@ -2,31 +2,25 @@ const express = require('express')
 const router = express.Router()
  let listOfService = [
     {
-        "id":1,
         "name":"Web Development"
     },
     {
-        "id":2,
+
         "name":"Mobile App Development"
     },
     {
-        "id":3,
         "name":"Digital Marketing"
     },
     {
-        "id":4,
         "name":"IOS App Development"
     },
     {
-        "id":5,
         "name":"Cloud Service"
     },
     {
-        "id":6,
         "name":"SaaS"
     },
     {
-        "id":7,
         "name":"Hardware Services"
     }
  ]
@@ -35,8 +29,9 @@ const router = express.Router()
  })
  router.param('id',(request,response,next,id)=>{
     // console.log(listOfService[request.params.id])
-    response.send(listOfService[request.params.id-1])
-   // next()
+    const service = listOfService[request.params.id-1]
+    response.send(service.name)
+   next()
 
 
  })
