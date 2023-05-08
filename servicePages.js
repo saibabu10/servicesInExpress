c265dd5a31fe0ad2c4e7f2a9ac8a3283074e6264
sai babu <saibabu@sais-Air-2.domain.name>
2023-05-08 19:02:21 +0530

    added 2 more fields in ejs file

--- servicePages.js
+++ servicePages.js
@@ -2,26 +2,40 @@ const express = require('express')
 const router = express.Router()
  let listOfService = [
     {
-        "name":"Web Development"
-    },
+        "name":"Web Development",
+        "cost":"$100",
+        "duration":'1'
+        },
     {
 
-        "name":"Mobile App Development"
+        "name":"Mobile App Development",
+        "cost":"$100",
+        "duration":'1'
+        },
+    {
+        "name":"Digital Marketing",
+        "cost":"$100",
+        "duration":'1'
     },
     {
-        "name":"Digital Marketing"
+        "name":"IOS App Development",
+        "cost":"$100",
+        "duration":'1'
     },
     {
-        "name":"IOS App Development"
+        "name":"Cloud Service",
+        "cost":"$100",
+        "duration":'1'
     },
     {
-        "name":"Cloud Service"
+        "name":"SaaS",
+        "cost":"$100",
+        "duration":'1'
     },
     {
-        "name":"SaaS"
-    },
-    {
-        "name":"Hardware Services"
+        "name":"Hardware Services",
+         "cost":"$100",
+        "duration":'1'
     }
  ]
  router.get('/',(request,response)=>{
@@ -32,13 +46,16 @@ const router = express.Router()
     response.render('new')
  })
  router.post('/',(request,response)=>{
-    listOfService.push({name:request.body.serviceName})
+    listOfService.push({name:request.body.serviceName,cost:request.body.serviceCost,Duration:request.body.serviceDuration})
     console.log(listOfService)
     response.redirect(`servicePages/${listOfService.length-1}`)
+})
 
- })
+
  router.get('/:id',(request,response)=>{
     const service = listOfService[request.params.id]
-    response.send(`The service ID is ${request.params.id} The service is ${service.name}`)
+     const dur = Number(service.duration)
+     console.log(dur)
+    response.send(`The service ID is ${request.params.id} The service is ${service.name} The service Cost is ${service.cost} and The service Duration is ${dur}`)
  })
  module.exports = router
