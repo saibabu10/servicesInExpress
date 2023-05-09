bfe1dc41fe33866719fb876ad78a025f686d789d
sai babu <saibabu@sais-MacBook-Air-2.local>
2023-05-09 09:13:29 +0530

    cleared duartion error

--- servicePages.js
+++ servicePages.js
@@ -4,38 +4,38 @@ const router = express.Router()
     {
         "name":"Web Development",
         "cost":"$100",
-        "duration":'1'
+        "duration":1
         },
     {
 
         "name":"Mobile App Development",
         "cost":"$100",
-        "duration":'1'
+        "duration":1
         },
     {
         "name":"Digital Marketing",
         "cost":"$100",
-        "duration":'1'
+        "duration":1
     },
     {
         "name":"IOS App Development",
         "cost":"$100",
-        "duration":'1'
+        "duration":1
     },
     {
         "name":"Cloud Service",
         "cost":"$100",
-        "duration":'1'
+        "duration":1
     },
     {
         "name":"SaaS",
         "cost":"$100",
-        "duration":'1'
+        "duration":1
     },
     {
         "name":"Hardware Services",
          "cost":"$100",
-        "duration":'1'
+        "duration":1
     }
  ]
  router.get('/',(request,response)=>{
@@ -46,7 +46,8 @@ const router = express.Router()
     response.render('new')
  })
  router.post('/',(request,response)=>{
-    listOfService.push({name:request.body.serviceName,cost:request.body.serviceCost,Duration:request.body.serviceDuration})
+    console.log(request.body.serviceDuration);
+    listOfService.push({name:request.body.serviceName,cost:request.body.serviceCost,duration:request.body.serviceDuration})
     console.log(listOfService)
     response.redirect(`servicePages/${listOfService.length-1}`)
 })
@@ -54,8 +55,9 @@ const router = express.Router()
 
  router.get('/:id',(request,response)=>{
     const service = listOfService[request.params.id]
-     const dur = Number(service.duration)
-     console.log(dur)
-    response.send(`The service ID is ${request.params.id} The service is ${service.name} The service Cost is ${service.cost} and The service Duration is ${dur}`)
+    //   const dur = String(service.duration)
+    //   console.log(dur)
+    response.send(`The service ID is ${request.params.id} The service is ${service.name} The service Cost is ${service.cost} and The service Duration is ${service.duration}`)
+
  })
  module.exports = router
